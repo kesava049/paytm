085954a26904429e00a01b0d0831243391656f0c
kalepallikesavulareddy@gmail.com <kalepallikesavulareddy@gmail.com>
2025-05-16 23:57:47 +0530

    two-functionalities-done

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,21 +1,27 @@
 import React from "react";
-import  useState  from "react"
 import  Button  from "./Button"
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 const Users = () => {
     // Replace with backend call
-    const [users, setUsers] = useState([{
-        firstName: "Harkirat",
-        lastName: "Singh",
-        _id: 1
-    }]);
+    const [users, setUsers] = useState([]);
+    const [filter, setFilter] = useState("");
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/v1/user/bulk?filter=" + filter)
+            .then((response) => {
+                setUsers(response.data.user || []);
+            })
+    }, [filter])
 
     return <>
         <div className="font-bold mt-6 text-lg">
             Users
         </div>
         <div className="my-2">
-            <input type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
+            <input onChange={(e)=>{
+                setFilter(e.target.value)
+            }}type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
         </div>
         <div>
             {users.map(user => <User user={user} />)}
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "../components/Heading";
 import SubHeading from "../components/SubHeading";
 import InputBox from "../components/InputBox";
 import Button from "../components/Button";
 import  BottomWarning  from "../components/BottomWarning";
-
+import axios from "axios";
 const Signup = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-xl shadow-lg">
                 <Heading label={"Signup"} />
                 <SubHeading label="Enter your details to create an account" />
-                <InputBox placeholder="ex:kesav" label={"First Name"} />
-                <InputBox placeholder="ex:kalepalli" label={"Last Name"} />
-                <InputBox placeholder="ex:kesav@gmail.com" label={"Email"} />
-                <InputBox placeholder="ex:1234567" label={"Password"} />
+                
+                <InputBox onChange= { (e) => {
+                    setFirstName(e.target.value)
+                }} placeholder="ex:kesav" label={"First Name"} />
+                <InputBox onChange={(e) => {
+                    setLastName(e.target.value)
+                }} placeholder="ex:kalepalli" label={"Last Name"} />
+                <InputBox onChange={(e) => {
+                    setUsername(e.target.value)
+                }} placeholder="ex:kesav@gmail.com" label={"Email"} />
+                <InputBox onChange={(e) => {
+                    setPassword(e.target.value)
+                }} placeholder="ex:1234567" label={"Password"} />
+
+
                 <div className="pt-4">
-                    <Button label={"Sign up"} />
+                    <Button onClick={()=>{
+                        axios.post("http://localhost:3000/api/v1/user/signup", {
+                            username,
+                            password,
+                            firstName,
+                            lastName,
+                        })
+                        
+                    }}label={"Sign up"} />
                 </div>
                 <BottomWarning label={"Already have an account?"} buttonText="Sign in" to="/signin"/>
             </div>
